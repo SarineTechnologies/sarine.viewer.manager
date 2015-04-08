2b88e108041ffff6bee64fcc8f1cb2a394760435
ran itzhaki <zamboney@gmail.com>
2015-04-08 15:09:28 +0300

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,26 @@
 'use strict';
 module.exports = function(grunt) {
+    var path = require("path");
+    var dirname = __dirname.split(path.sep)
+    dirname.pop();
+    dirname = dirname.pop();
+    var branch = "dev"
     require('load-grunt-tasks')(grunt)
-    var files = ["Gruntfile.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", "release.cmd", "commit.cmd"]
+    var files = ["Gruntfile.js", "copyright.txt", "GruntfileBundle.js", "package.json", "dist/*.js", "coffee/*.coffee", "bower.json", "release.cmd", "commit.cmd"]
     var message = "commit"
     grunt.initConfig({
         config: grunt.file.readJSON("bower.json"),
         version: {
             project: {
                 src: ['bower.json', 'package.json']
+            }
+        },
+        gitcheckout: {
+            task: {
+                options: {
+                    branch: "<%= branch %>",
+                    overwrite: true
+                }
             }
         },
         gitcommit: {
@@ -19,11 +32,21 @@ module.exports = function(grunt) {
                 files: {
                     src: files
                 }
+            },
+            bower: {
+                options: {
+                    message: "release : <%= config.version %>",
+                    force: true
+                },
+                files: {
+                    src: ["bower.json", "package.json"]
+                }
             }
         },
         gitpush: {
             all: {
                 options: {
+                    branch: "<%= branch %>",
                     force: true
                 },
                 files: {
@@ -55,15 +78,29 @@ module.exports = function(grunt) {
             all: {
                 options: {
                     questions: [{
+                        config: 'config.customBranch',
+                        type: 'confirm',
+                        message: 'create new branch base on the folder name - ' + dirname
+                    }, {
                         config: 'config.message',
                         type: 'input',
                         message: 'comment:\n',
                         default: 'commit'
-                    }]
+                    }],
+                    then: function(results, done) {
+                        grunt.log.writeln(results["config.customBranch"])
+                        grunt.config.set('branch', 'dev')
+                        if (results["config.customBranch"]) {
+                            grunt.config.set('branch', dirname);
+                            grunt.task.run('gitcheckout:task');
+                        }
+                        done();
+                        return true;
+                    }
                 }
             }
         }
     })
-    grunt.registerTask('commit', ['prompt', 'gitadd', 'gitcommit', 'gitpush']);
-    grunt.registerTask('release-git', ['version:project:patch', 'commit', 'release']);
+    grunt.registerTask('commit', ['prompt', 'gitadd', 'gitcommit:all', 'gitpush']);
+    grunt.registerTask('release-git', ['release:' + grunt.file.readJSON("bower.json")["version"]]);
 };
