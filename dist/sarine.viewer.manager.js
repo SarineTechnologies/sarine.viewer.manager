42874baa81fb27f7d67daa0efa942f71feba8283
SarineColor <github@sarine.com>
2015-06-17 17:24:11 +0300

    deploy 0.3.0

--- dist/sarine.viewer.manager.js
+++ dist/sarine.viewer.manager.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.manager - v0.2.1 -  Wednesday, June 17th, 2015, 5:11:49 PM 
+sarine.viewer.manager - v0.3.0 -  Wednesday, June 17th, 2015, 5:24:00 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -39,7 +39,7 @@ sarine.viewer.manager - v0.2.1 -  Wednesday, June 17th, 2015, 5:11:49 PM
 
     function ViewerManger(option) {
       fromTag = option.fromTag, toTag = option.toTag, stoneViews = option.stoneViews, template = option.template, jsons = option.jsons, logicRoot = option.logicRoot;
-      window.cacheVersion = "?" + "__VERSION__";
+      window.cacheVersion = "?" + "0.3.0";
       if (configuration.cacheVersion) {
         window.cacheVersion += configuration.cacheVersion;
       }
