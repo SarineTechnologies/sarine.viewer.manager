8ffdc8b2ab37b2983299743ce9e6f8819ea364d9
SarineColor <github@sarine.com>
2015-11-18 14:20:40 +0200

    deploy 0.9.0

--- coffee/sarine.viewer.manager.coffee
+++ coffee/sarine.viewer.manager.coffee
@@ -1,6 +1,7 @@
 ###!
-sarine.viewer.manager - v0.8.0 -  Wednesday, November 18th, 2015, 2:13:48 PM 
+sarine.viewer.manager - v0.9.0 -  Wednesday, November 18th, 2015, 2:20:36 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+
 ###
 class ViewerManger
 	viewers  = []
--- dist/sarine.viewer.manager.js
+++ dist/sarine.viewer.manager.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.manager - v0.8.0 -  Wednesday, November 18th, 2015, 2:13:48 PM 
+sarine.viewer.manager - v0.9.0 -  Wednesday, November 18th, 2015, 2:20:36 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -41,7 +41,7 @@ sarine.viewer.manager - v0.8.0 -  Wednesday, November 18th, 2015, 2:13:48 PM
 
     function ViewerManger(option) {
       fromTag = option.fromTag, toTag = option.toTag, stoneViews = option.stoneViews, template = option.template, jsons = option.jsons, logicRoot = option.logicRoot;
-      window.cacheVersion = "?" + "__VERSION__";
+      window.cacheVersion = "?" + "0.9.0";
       if (configuration.cacheVersion) {
         window.cacheVersion += configuration.cacheVersion;
       }
