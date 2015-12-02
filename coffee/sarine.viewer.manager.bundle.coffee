2bf22efd292f71ada04d02ba3a6e97b6d3ff9df8
SarineColor <github@sarine.com>
2015-12-02 13:37:02 +0200

    deploy 0.10.0

--- coffee/sarine.viewer.manager.bundle.coffee
+++ coffee/sarine.viewer.manager.bundle.coffee
@@ -1,6 +1,7 @@
 ###!
-sarine.viewer.manager - v0.9.0 -  Wednesday, December 2nd, 2015, 1:32:35 PM 
+sarine.viewer.manager - v0.10.0 -  Wednesday, December 2nd, 2015, 1:37:00 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
+
 ###
 
 class ViewerManger
@@ -22,7 +23,7 @@ class ViewerManger
 
 	constructor: (option) ->
 		{fromTag, toTag, stoneViews,template,jsons,logicRoot} = option
-		window.cacheVersion = "?" +  "__VERSION__"
+		window.cacheVersion = "?" +  "0.10.0"
 		if configuration.cacheVersion
 			window.cacheVersion += configuration.cacheVersion
 		logicRoot = stoneViews.viewersBaseUrl + "atomic/{version}/js/"
--- dist/sarine.viewer.manager.bundle.js
+++ dist/sarine.viewer.manager.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.manager - v0.9.0 -  Wednesday, December 2nd, 2015, 1:32:35 PM 
+sarine.viewer.manager - v0.10.0 -  Wednesday, December 2nd, 2015, 1:37:00 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -41,7 +41,7 @@ sarine.viewer.manager - v0.9.0 -  Wednesday, December 2nd, 2015, 1:32:35 PM
 
     function ViewerManger(option) {
       fromTag = option.fromTag, toTag = option.toTag, stoneViews = option.stoneViews, template = option.template, jsons = option.jsons, logicRoot = option.logicRoot;
-      window.cacheVersion = "?" + "__VERSION__";
+      window.cacheVersion = "?" + "0.10.0";
       if (configuration.cacheVersion) {
         window.cacheVersion += configuration.cacheVersion;
       }
