6c199cba618b95dd6a159cb404633a60fb68ddd1
Lucky Luke <pelyem@gmail.com>
2017-04-27 09:00:19 +0300

    1. delete code that update banner in coffee file 2. delete code that produce coffee bundle 3. compilation only convert to js + change version + grunt-contrib-uglify (=min + banner + remove comments + map) 4. delete dist from remote

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -1,152 +1,55 @@
-'use strict';
+'use strict'
 module.exports = function(grunt) {
+
     require('load-grunt-tasks')(grunt)
-    var target = grunt.option('target') || "";
-    grunt
+    
     grunt.initConfig({
-        config: grunt.file.readJSON(target + "bower.json"),
+        config: grunt.file.readJSON("package.json"),
         clean: {
-            build: [target + "lib/", target + "dist/", target + "build/"],
-            bundlecoffee: [target + "coffee/*.bundle.coffee"],
-            postbuild: [target + "build/"]
+            build: ["dist/"]
+        },
+        coffee: {
+            compile: {
+                options: {
+                    sourceMap: true,
+                },
+                files: {
+                    'dist/<%= config.name %>.js' : 'coffee/*.coffee' // convert to java-script
+                }
+            }
         },
         changeVersion: {
-            coffeeBundle: {
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee'
-            },
             files: {
-                src: [target + 'dist/<%= config.name %>.js'],
-                dest: target + 'dist/<%= config.name %>.js'
-            }
-        },
-        commentsCoffee: {
-            coffee: {
-                src: [target + 'coffee/<%= config.name %>.coffee'],
-                dest: target + 'coffee/<%= config.name %>.coffee',
-            },
-            coffeeBundle: {
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
-            },
-        },
-        concat: {
-            coffee: {
-                options: {
-                    stripBanners: true,
-                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
-                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###',
-                },
-                src: [target + 'coffee/<%= config.name %>.coffee'],
-                dest: target + 'coffee/<%= config.name %>.coffee',
-            },
-            coffeebundle: {
-                options: {
-                    stripBanners: true,
-                    banner: '###!\n<%= config.name %> - v<%= config.version %> - ' +
-                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n###\n',
-                },
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee',
-            }
-        },
-        bower: {
-            install: {
-                verbose: true,
-                bowerOptions: {
-                    forceLatest: true
-                },
-                options: {
-                    targetDir: target + "lib",
-                    layout: function(type, component, source) {
-                        var renamedType = type;
-                        if (source.indexOf("bundle") == -1 &&
-                            type == 'coffee' &&
-                            grunt.file.readJSON(target + "bower.json").dependencies &&
-                            Object.getOwnPropertyNames(grunt.file.readJSON(target + "bower.json").dependencies).indexOf(component) != -1)
-                            return "add";
-                        else
-                            return "remove";
-                    }
-                }
+                src: 'dist/<%= config.name %>.js',
+                dest: 'dist/<%= config.name %>.js'
             }
         },
         uglify: {
             options: {
-                preserveComments: 'some',
-                sourceMap : true
+                banner: '/*\n<%= config.name %> - v<%= config.version %> - ' +
+                        ' <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> ' + '\n ' + grunt.file.read("copyright.txt") + '\n*/',
+                preserveComments: false,
+                sourceMap : true,
+                sourceMapIn: "dist/<%= config.name %>.js.map"
             },
             build: {
-                src: target + 'dist/<%= config.name %>.js',
-                dest: target + 'dist/<%= config.name %>.min.js'
-            },
-            bundle: {
-                src: target + 'dist/<%= config.name %>.bundle.js',
-                dest: target + 'dist/<%= config.name %>.bundle.min.js'
-            }
-        },
-        coffeescript_concat: {
-            bundle: {
-                src: [target + 'lib/add/*.coffee', target + 'coffee/*.coffee', target + '!coffee/*.bundle.coffee'],
-                dest: target + 'coffee/<%= config.name %>.bundle.coffee'
-
-            }
-        },
-        coffee: {
-            build: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                dest: target + 'dist/<%= config.name %>.js',
-                src: [target + 'coffee/<%= config.name %>.coffee']
-
-            },
-            bundle: {
-                option: {
-                    join: true,
-                    extDot: 'last'
-                },
-                dest: target + 'dist/<%= config.name %>.bundle.js',
-                src: [target + 'coffee/<%= config.name %>.bundle.coffee']
-
+                src: 'dist/<%= config.name %>.js',
+                dest: 'dist/<%= config.name %>.min.js'
             }
         }
-    })
+    });
+
     grunt.registerTask('bundle', [
         'clean:build',
-        'clean:bundlecoffee',
-        'bower',
-        'coffeescript_concat',
-        'commentsCoffee:coffeeBundle',
-        'concat:coffeebundle',
-        'changeVersion:coffeeBundle',
-        'coffee:bundle',
-        'commentsCoffee:coffee',
-        'concat:coffee',
-        'coffee:build',
-        'changeVersion:files',
-        'uglify',
-        'clean:postbuild'
+        'coffee',// Compile CoffeeScript files to JavaScript + map
+        'changeVersion',
+        'uglify',//min + banner + remove comments + map    
     ]);
-    grunt.registerMultiTask('changeVersion', 'Remove comments from production code', function() {
+    
+    grunt.registerMultiTask('changeVersion', 'Inject version from package.json into JS files', function() {
         this.files[0].src.forEach(function(file) {
             var contents = grunt.file.read(file);
-            contents = contents.replace("__VERSION__", grunt.file.readJSON(target + "bower.json").version)
-            grunt.file.write(file, contents);
-
-        });
-
-    })
-    grunt.registerMultiTask('commentsCoffee', 'Remove comments from production code', function() {
-        this.files[0].src.forEach(function(file) {
-            var contents = grunt.file.read(file);
-            if (contents.match(/###!([\s\S]*?)###[\s\S]*?/gm))
-                contents = contents.replace(/###!([\s\S]*?)###[\s\S]*?/gm, "");
-            else {
-
-                contents = contents
-            }
+            contents = contents.replace("__VERSION__", grunt.file.readJSON("package.json").version)
             grunt.file.write(file, contents);
         });
     });
--- coffee/sarine.viewer.manager.coffee
+++ coffee/sarine.viewer.manager.coffee
@@ -1,8 +1,3 @@
-###!
-sarine.viewer.manager - v0.19.0 -  Sunday, February 19th, 2017, 5:23:00 PM 
- The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
-
-###
 class ViewerManger
 	viewers  = []
 	stoneViews = undefined
