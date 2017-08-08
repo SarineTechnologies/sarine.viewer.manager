5d7696ad5069cdf2e4d21f177a63665a95acb4e6
Alex Savenok <sawenok@gmail.com>
2017-08-08 17:15:05 +0300

    removed checking of configuration.experiences in addViewer()

--- dist/sarine.viewer.manager.bundle.js
+++ dist/sarine.viewer.manager.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.manager - v0.19.0 -  Tuesday, July 11th, 2017, 2:20:39 PM 
+sarine.viewer.manager - v0.19.0 -  Tuesday, August 8th, 2017, 3:55:06 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -289,9 +289,6 @@ sarine.viewer.manager - v0.19.0 -  Tuesday, July 11th, 2017, 2:20:39 PM
       $.ajaxSetup({
         async: false
       });
-      if (configuration.experiences && typeof configuration.experiences !== 'undefined' && !existInConfig(type)) {
-        return;
-      }
       if (jsonsAllObj === void 0) {
         $.getJSON(jsonsAll + window.cacheVersion, (function(_this) {
           return function(d) {
